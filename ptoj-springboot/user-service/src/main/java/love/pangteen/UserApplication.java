78f53da07e39693ac8af92beb33a01cc955ee64d
PangTeen <1687289734@qq.com>
2024-01-12 22:24:31 +0800

    初始化工程

---
+++ ptoj-springboot/user-service/src/main/java/love/pangteen/UserApplication.java
@@ -0,0 +1,20 @@
+package love.pangteen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: PTOJ
+ * @author: PangTeen
+ * @create: 2024/1/12 22:15
+ **/
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
