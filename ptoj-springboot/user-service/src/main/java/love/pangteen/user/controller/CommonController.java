500a7b778953fef8b428fcfbb0f639bca31cb80e
PangTeen <1687289734@qq.com>
2024-03-15 16:47:42 +0800

    RocketMQ docker安装

---
+++ ptoj-springboot/user-service/src/main/java/love/pangteen/user/controller/CommonController.java
@@ -0,0 +1,25 @@
+package love.pangteen.user.controller;
+
+import love.pangteen.result.CommonResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @program: PTOJ
+ * @author: PangTeen
+ * @create: 2024/3/12 22:47
+ **/
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+//    @GetMapping("/get-website-config")
+//    public CommonResult<Map<Object, Object>> getWebConfig() {
+//        return homeService.getWebConfig();
+//    }
+    
+}
